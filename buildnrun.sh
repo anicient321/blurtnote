b8ff5da7417309c98b76e380da65fa228b3cd164
anicient321 <79281571+anicient321@users.noreply.github.com>
2026-01-18 18:27:45 +0100

    Update buildnrun.sh
    fixed deps

--- buildnrun.sh
+++ buildnrun.sh
@@ -8,12 +8,24 @@ pkill -fe blurtnote
 rm -rfv build
 mkdir build
 
+#!/bin/bash
+
 echo -e "+ installing build dependencies..."
-(command -v apt >/dev/null 2>&1 && sudo apt update -qq >/dev/null 2>&1 && sudo apt install -yqq g++ cmake ninja-build qt6-base-dev qt6-tools-dev qt6-base-dev-tools >/dev/null 2>&1 || \
- command -v dnf >/dev/null 2>&1 && sudo dnf install -y gcc-c++ cmake ninja-build qt6-qtbase-devel qt6-qttools-devel >/dev/null 2>&1 || \
- command -v pacman >/dev/null 2>&1 && sudo pacman -S --noconfirm gcc cmake ninja qt6-base qt6-tools >/dev/null 2>&1 || \
- command -v zypper >/dev/null 2>&1 && sudo zypper install -y gcc-c++ cmake ninja qt6-base-devel qt6-tools-devel >/dev/null 2>&1 || \
- command -v apk >/dev/null 2>&1 && sudo apk add g++ cmake ninja qt6-qtbase-dev qt6-qttools-libs >/dev/null 2>&1)
+if command -v apt >/dev/null 2>&1; then
+    sudo apt update -qq >/dev/null 2>>"err.log" || echo "apt update failed, see err.log"
+    sudo apt install -yqq g++ cmake qt6-base-dev qt6-tools-dev qt6-base-dev-tools >/dev/null 2>>"err.log" || echo "apt install failed, see err.log"
+elif command -v dnf >/dev/null 2>&1; then
+    sudo dnf install -y gcc-c++ cmake qt6-qtbase-devel qt6-qttools-devel >/dev/null 2>>"err.log" || echo "dnf install failed, see err.log"
+elif command -v pacman >/dev/null 2>&1; then
+    sudo pacman -S --noconfirm gcc cmake qt6-base qt6-tools >/dev/null 2>>"err.log" || echo "pacman install failed, see err.log"
+elif command -v zypper >/dev/null 2>&1; then
+    sudo zypper install -y gcc-c++ cmake qt6-base-devel qt6-tools-devel >/dev/null 2>>"err.log" || echo "zypper install failed, see err.log"
+elif command -v apk >/dev/null 2>&1; then
+    sudo apk add g++ cmake qt6-qtbase-dev qt6-qttools-libs >/dev/null 2>>"err.log" || echo "apk install failed, see err.log"
+else
+    echo "- something went wrong :( see $PWD/err.log for details"
+    exit 1
+fi
 
 echo -e '+ building blurtnote for linux'
 cmake -B build -S src -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu
