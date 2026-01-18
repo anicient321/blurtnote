fd29a1fd2f1545c0844194cd737af9c7d03ff7e1
anicient321 <79281571+anicient321@users.noreply.github.com>
2026-01-18 23:29:52 +0100

    Update uninstall.sh
    add pkill

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,7 @@ echo -e "blurtnote UNinstaller\n\n+ uninstalling blurtnote from user $USER"
 
 echo -e '+ need SU rights to uninstall...\n'
 sudo -v
+pkill -f blurtnote
 sudo rm -rf "$ICONDIR/notes.svg"
 sudo rm -rf "$DESKTOPDIR/blurtnote.desktop"
 sudo rm -rf /opt/blurtnote
